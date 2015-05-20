680105227d73db138c1a821c93f254c43e226213
Young Lee <younglee.phd@gmail>
2015-05-20 18:56:17 -0500

    Sort the result by similarity

--- recommender.py
+++ recommender.py
@@ -7,11 +7,12 @@ class Recommender(MRJob):
     def steps(self):
         return [
             MRStep(mapper = self.individual_ratings, reducer = self.ratings_by_user),
-            MRStep(mapper = self.item_pairs, reducer = self.similarity)
+            MRStep(mapper = self.item_pairs, reducer = self.similarity),
+            MRStep(mapper = self.order_by_similarity, reducer = self.output)
         ]
         
     def individual_ratings(self, _, line):
-        user_id, item_id, rating = line.split('|')
+        user_id, item_id, rating, _ = line.split('\t')
         yield user_id, (item_id, rating)
 
     def ratings_by_user(self, user_id, values):
@@ -34,7 +35,7 @@ class Recommender(MRJob):
         sum_xx = 0
         sum_yy = 0
         sum_xy = 0
-        num_pairs = 0
+        occurrence = 0
         for x, y in rating_pair:
             x = int(x)
             y = int(y)
@@ -43,12 +44,22 @@ class Recommender(MRJob):
             sum_xy += x*y
             sum_x += x
             sum_y += y
-            num_pairs += 1
+            occurrence += 1
         
-        denom = sqrt(num_pairs*sum_xx - sum_x*sum_x)*sqrt(num_pairs*sum_yy - sum_y*sum_y)
-        correlation = (num_pairs*sum_xy - sum_x*sum_y)/denom if denom else 0.0
+        denom = sqrt(occurrence*sum_xx - sum_x*sum_x)*sqrt(occurrence*sum_yy - sum_y*sum_y)
+        correlation = (occurrence*sum_xy - sum_x*sum_y)/denom if denom else 0.0
+        correlation = (correlation + 1.0)/2.0
         
-        yield item_pair, correlation
+        yield item_pair, (correlation, occurrence)
         
+    def order_by_similarity(self, item_pair, values):
+        similarity, occurrence = values
+        yield (item_pair[0], similarity), (item_pair[1], occurrence)
+        
+    def output(self, key, values):
+        item1, similarity = key
+        for item2, occurrence in values:
+            yield None, (item1, item2, similarity, occurrence)
+
 if __name__ == '__main__':
     Recommender.run()
